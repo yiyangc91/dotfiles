fd5adb5f0ce09c3200b8373ba0d9f6a5ffcfdc07
Yiyang Chen <yiyangc91@gmail.com>
2019-05-19 16:42:37 +1000

    something lock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sed -i 's/^fade:.*/fade: False/' $HOME/.xscreensaver && xscreensaver-command -lock
 
-xscreensaver-command -lock
+# set the fade timer back after a short sleep
+# this is important because the config reloading doesnt work otherwise
+sleep 1
+sed -i 's/^fade:.*$/fade: True/' $HOME/.xscreensaver
