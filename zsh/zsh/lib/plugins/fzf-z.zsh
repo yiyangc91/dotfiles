03838114743b5cfa12146b763063380ffbadc786
Yiyang Chen <yiyangc91@gmail.com>
2020-05-09 14:32:47 -0700

    More changes wow

---
+++ zsh/zsh/lib/plugins/fzf-z.zsh
@@ -0,0 +1,4 @@
+# Prevents fzf-z from adding subdirectories to the output.
+# This is actually quite annoying. Why not just ALT+C?
+export FZFZ_SUBDIR_LIMIT=0
+export FZFZ_EXTRA_OPTS='--preview-window=right:hidden'
